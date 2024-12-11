272779d816faed91b729e46267a937ff8a6a35e8
nezmar48 <132996390+nezmar48@users.noreply.github.com>
2024-12-11 14:43:42 +0100

    formating fix and com1 restore on start (#21)
    Co-authored-by: Nezmar <nezmar@TondaLinux.localdomain>

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -2,15 +2,9 @@
 #include "output/serial_port.h"
 #include "output/frame_buffer.h"
 
-extern "C" int kmain(){
-    //frame buffer test
-    // max 14 characters
-    // at 15 (0) [0x00000010003e] 0008:000000000010003e (unk. ctxt): movdqa xmm0, xmmword ptr ds:[ebx-4232] ; 660f6f8378efffff
-    // causes int 6
-    // tis is called during the fb_write function call not during it's execution
-    // at 14 char 
-    // (0) [0x000000100040] 0008:0000000000100040 (unk. ctxt): mov dword ptr ss:[ebp-23], 0x34333231 ; c745e931323334
+extern "C" int kmain() {
 
+    //frame buffer test
     char buffer[] = "123456789012345";
 	    fb_write(buffer,sizeof(buffer),LIGHT_GREEN,BLACK);
      
