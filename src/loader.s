0f596b0861934c661d2c87f1cf7cc2e030c3da03
nezmar48 <132996390+nezmar48@users.noreply.github.com>
2025-01-15 15:37:49 +0100

    grub modules (#25)
    * update makefile for modules
    
    * push multiboot pointer to stack
    
    * call external program
    
    ---------
    
    Co-authored-by: Nezmar <nezmar@TondaLinux.localdomain>

--- src/loader.s
+++ src/loader.s
@@ -1,10 +1,6 @@
-global loader                   ; the entry symbol for ELF
+global loader                       ; the entry symbol for ELF
 
-MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-FLAGS        equ 0x0            ; multiboot flags
-CHECKSUM     equ - MAGIC_NUMBER  ; calculate the  
-
-KERNEL_STACK_SIZE equ 4096      
+KERNEL_STACK_SIZE equ 0x1000        ; 4 KB      
 
 section .bss
 align 4
@@ -12,13 +8,14 @@ kernel_stack:
     resb KERNEL_STACK_SIZE
 
 section .text
-align 4                         ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER
-    dd FLAGS
-    dd CHECKSUM 
     
-loader:   
-    cli            ; disable interrupts
+loader:
+    cli         ; disable interrupts
+    
+    mov ebp, kernel_stack + KERNEL_STACK_SIZE   ; set up stack
+    mov esp, ebp
+
+    push ebx    ; push adress of multiboot strucure
 
     extern create_gdt
     call create_gdt
@@ -27,14 +24,11 @@ loader:
     extern idt_init
     call idt_init
     lidt [eax]
-
     sti            ; enable interrupts
-
     int 32 ; test interrupts
 
     ; protected mode is already enabled, DS is at 0x10
-
-    mov esp, kernel_stack + KERNEL_STACK_SIZE
+    
     extern kmain
     call kmain
 .loop:
